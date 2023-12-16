3fcaecb5731b5d88e3b34156fd914b0cd2b5ebe8
SurajSanjaySingh <singhsurajs1311@gmail.com>
2023-12-15 19:21:58 -0500

    update

--- public/js/login_signup.js
+++ public/js/login_signup.js
@@ -1,9 +1,15 @@
 const loginBtn = document.querySelector(".login-button");
 const loginPopup = document.querySelector(".login-popup");
 const overlay = document.querySelector(".overlay-bg");
-console.log("working");
+const registerBtn = document.querySelector('.register-button')
+const registerPopup = document.querySelector('.register-popup')
 loginBtn.addEventListener("click", () => {
   console.log("clicked");
   loginPopup.classList.toggle("show-login");
   overlay.classList.toggle("show-overlay");
 });
+console.log(registerBtn)
+registerBtn.addEventListener('click',()=>{
+  console.log("hi")
+  registerPopup.classList.toggle('show-register');
+})
